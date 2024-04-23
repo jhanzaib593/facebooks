38d3a185f3c8f004f8f5f79e05e721bcc78bca24
Jhanzaib <jhanzaib593@gmail.com>
2024-04-23 09:59:31 +0500

    acc

--- src/components/Layout/index.js
+++ src/components/Layout/index.js
@@ -4,7 +4,7 @@ import Login from "../../pages/Login";
 
 const Layout = () => {
   const user = localStorage.getItem("soical_user");
-  // const user = false
+  // const user = false;
 
   return <div>{user ? <Home /> : <Login />}</div>;
 };
